86b221d53cb380c65aa4dcfc5d77bf9afa477e85
Alexandra Yakovleva <leksea@gmail.com>
2025-12-06 18:45:11 -0800

    Updated userprofile
    Added hashing

--- modules/userprofile.py
+++ modules/userprofile.py
@@ -4,6 +4,7 @@ Author: Alexandra Yakovleva
 
 Defines the UserProfile class for the social network.
 """
+from typing import Any
 
 
 class UserProfile:
@@ -14,7 +15,7 @@ class UserProfile:
         self.email = email
         self.phone = phone
 
-    def update(self, name:str=None, email:str=None, phone:str=None):
+    def update(self, name:str=None, email:str=None, phone:str=None) -> None:
         """Update fields that are not None."""
         if name is not None and name != "":
             self.name = name
@@ -24,8 +25,23 @@ class UserProfile:
             self.phone = phone
 
     def __str__(self):
+        """Return a string representation of the user."""
         return (f"Name:  {self.name}\n"
                 f"Email: {self.email}\n"
                 f"Phone: {self.phone}")
-    def __eq__(self, other) -> bool:
-        return (self.name, self.email, self.phone) == (other.name, other.email, other.phone)
+
+    def __eq__(self, other: Any) -> bool:
+        """Two UserProfiles are equal if all identifying fields are equal."""
+        if not isinstance(other, UserProfile):
+            return False
+        return (
+            self.name == other.name and
+            self.email == other.email and
+            self.phone == other.phone
+        )
+    def __repr__(self) -> str:
+        return f"UserProfile(name={self.name!r}, email={self.email!r}, phone={self.phone!r})"
+
+    def __hash__(self) -> int:
+       """Return a hash of the user's profile to be used with dicts and sets"""
+       return hash((self.name, self.email, self.phone))
